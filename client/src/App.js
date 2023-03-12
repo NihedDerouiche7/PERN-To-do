e998170a91b12bc80a92df0d42869534a4cc293a
Nihed Derouiche <deroucihenihed@gmail.com>
2023-03-12 12:58:05 +0100

    list todo

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import InputTodo from './components/InputTodo';
+import ListTodo from './components/ListTodo';
 
 function App() {
   return (   
   <>
   <div className='container'>
   <InputTodo/>
+  <ListTodo/>
   </div>
 
   </>
--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -1,1 +1,50 @@
+ import React, { useEffect ,useState} from "react"
+
+ 
+ const ListTodo = () => {
+
+    const [todos,setTodos]= useState([])
+
+const gettodos =async ()=>{
+try {
+    const response = await fetch("http://localhost:5000/todos");
+       const jsonData = await response.json()
+      setTodos(jsonData)
+    
+} catch (err) {
+    console.error(err.message);
+}
+}
+
+    useEffect (()=>{
+        gettodos();
+    },[])
+    console.log(todos)
+   return (
+     <>
+      <table className="table mt-5 text-center">
+    <thead>
+      <tr>
+        <th>description</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      {todos.map(todo=>(
+        <tr>
+            <td>{todo.description}</td>
+            <td>Edit</td>
+            <td>Delete</td>
+        </tr>
+      ))}
+      
+     
+    </tbody>
+  </table>
+     </>
+   )
+ }
+ 
+ export default ListTodo
  
